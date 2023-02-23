d4b3bcd39d26471df74bf318daf2f9100c5bb3cd
Bart Agreda <114963139+bagreda-vmw@users.noreply.github.com>
2023-02-23 19:44:58 +0800

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu + Coles!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu + Coles!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
     }
 }
