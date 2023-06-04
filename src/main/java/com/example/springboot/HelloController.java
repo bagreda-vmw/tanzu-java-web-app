e747a193b47cd5f951037eb4098e5c4b08fd6579
bagreda-vmw <bagreda@vmware.com>
2023-06-04 13:15:38 +1000

    change greeting

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Bart!";
+		return "Greetings from VMware Tanzu!";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Bart!", controller.index());
+        assertEquals("Greetings from VMware Tanzu!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Bart!"));
+            .andExpect(content().string("Greetings from VMware Tanzu"));
     }
 }
