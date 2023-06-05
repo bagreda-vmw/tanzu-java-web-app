723c597f93b4d9a5285e26dc6b84342cbfeb96db
bagreda-vmw <bagreda@vmware.com>
2023-06-05 14:57:20 +1000

    update greeting

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Springboot!", controller.index());
+        assertEquals("Greetings from VMware Tanzu!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Springboot!"));
+            .andExpect(content().string("Greetings from VMware Tanzu!"));
     }
 }
