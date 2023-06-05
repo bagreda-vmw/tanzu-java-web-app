c4bbe5ec7e8af81ffeac0553d4f4200a8f868722
bagreda-vmw <bagreda@vmware.com>
2023-06-05 15:09:07 +1000

    update greeting

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from VMware Tanzu!";
+		return "Greetings from VMware Tanzu Application Platform!";
 	}
 
 }
